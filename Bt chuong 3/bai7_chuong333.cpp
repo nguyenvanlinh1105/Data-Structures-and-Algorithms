5a4e1e798e311c062bfd008678adf8b739e36cdf
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2023-12-21 00:44:23 +0700

    update file bai 7 chuong 333

--- Bt chuong 3/bai7_chuong333.cpp
+++ Bt chuong 3/bai7_chuong333.cpp
@@ -49,6 +49,29 @@ struct Node* insert(struct Node* root, struct SV sv) {
     return root;
 }
 
+struct Node* insert1(struct Node* root, Node* p) {
+    struct Node* tmp,* TMP;
+
+    if (root == NULL) {
+        root = p;
+    } else {
+        tmp = root;
+        while (tmp != NULL) {
+            TMP = tmp;
+            if (strcmp(p->sv.maSV, tmp->sv.maSV) < 0)
+                tmp = tmp->L;
+            else 
+                tmp = tmp->R;
+        }
+        if (strcmp(p->sv.maSV, TMP->sv.maSV) < 0) {
+            TMP->L = p;
+        } else {
+            TMP->R = p;
+        }
+    }
+    return root;
+}
+
 void printStudent(struct SV student) {
     printf("Student ID: %s\n", student.maSV);
     printf("Student Name: %s\n", student.hoTen);
@@ -102,7 +125,7 @@ void DemSV5_5(struct Node* root, int& count) {
         DemSV5_5(root->L, count);
         
         if (root->sv.dtb > 5.5) {
-            (*count)++;
+            (count)++;
         }
         DemSV5_5(root->R, count);
     }
@@ -123,21 +146,180 @@ void printStudentsAbove5_5(struct Node* root) {
         printStudentsAbove5_5(root->R);
     }
 }
-// bài 10: 
+// //bài 10: 
 
-// Function to print the tree using inorder traversal
-//void inorderTraversal(struct Node* root) {
-//    if (root != NULL) {
-//        inorderTraversal(root->L);
-//        printf("%s ", root->sv.maSV);
-//        inorderTraversal(root->R);
-//    }
-//}
+struct Node* TKIEM_2(struct Node* Cay, char maSV[]) {
+    struct Node* temp = Cay;
+    while (temp != NULL && strcmp(maSV, temp->sv.maSV) != 0) {
+        if (strcmp(maSV, temp->sv.maSV) < 0) {
+            temp = temp->L;
+        } else {
+            temp = temp->R;
+        }
+    }
+
+    if (temp != NULL) {
+        return temp;
+    } else {
+        return NULL;
+    }
+}
+Node *XOAGOC(Node *Cay)
+{
+	Node *tg, *CayL, *CayR;
+	
+	if (Cay->L == NULL & Cay->R == NULL)
+		Cay = NULL;
+	else
+		if (Cay->L != NULL & Cay->R == NULL)//Cay lech trai
+		{
+			tg = Cay;
+			Cay = Cay->L;
+			tg->L = NULL;	
+		}
+		else
+			if (Cay->L == NULL & Cay->R != NULL)//Cay lech phai
+			{
+				tg = Cay;
+				Cay = Cay->R;
+				tg->R = NULL;	
+			}
+			else // cay can bàng
+			{				
+				tg = Cay;
+				CayR = Cay->R;
+				Cay = Cay->L ;
+				Cay = insert1(Cay, CayR);
+			}		
+	return Cay;	 
+}
+Node *TRUOC(Node *Cay, Node *nut)
+{
+	Node *tr;
+	tr = Cay;
+	while (tr->L!=nut && tr->R!= nut)
+		if (strcmp(nut->sv.maSV, tr->sv.maSV)<0)
+			tr = tr->L;
+		else
+			tr = tr->R;
+	return tr;			
+}
+Node *XOA_LA(Node *Cay, Node *nut)
+{
+	Node *tr;
+	tr = TRUOC(Cay, nut);
+	if (tr->R==nut)
+		tr->R=NULL;
+	else
+		tr->L =NULL;
+	return Cay;				
+}
+Node *XOA_TG_T(Node *Cay, Node *nut)
+{
+	Node *tr;
+	tr = TRUOC(Cay, nut);
+	if (tr->R == nut)
+	{
+		tr->R = nut->L;
+		nut->L = NULL;
+	}
+	else
+	{
+		tr->L = nut->L;
+		nut->L = NULL;
+	}
+	return Cay;			
+}
+Node *XOA_TG_P(Node *Cay, Node *nut)
+{
+	Node *tr;
+	tr = TRUOC(Cay, nut);
+	if (tr->L ==nut)
+	{
+		tr->L = nut->R;
+		nut->R = NULL;
+	}
+	else
+	{
+		tr->R = nut->R;
+		nut->R = NULL;	
+	}
+	return Cay;
+}
+Node *XOA_TG(Node *Cay, Node *nut)
+{
+	Node *tr, *CayR;
+	tr = TRUOC(Cay, nut);
+	if (tr->L ==nut)
+	{
+		tr->L = nut->L;
+		nut->L = NULL;
+		CayR= nut->R;
+		nut->R = NULL;
+		Cay = insert1(Cay, CayR);			
+	}
+	else
+	{
+		tr->R = nut->L;
+		nut->L =NULL;
+		CayR= nut->R;
+		nut->R = NULL;
+		Cay = insert1(Cay, CayR);			
+	}
+	return Cay;
+}
+Node *XOA(Node *Cay, SV sv)
+{
+	Node *tk;
+	while (TKIEM_2(Cay, sv.maSV)!=NULL)
+	{
+		tk = TKIEM_2(Cay, sv.maSV);
+		if (tk ==Cay)
+			Cay = XOAGOC(Cay);
+		else
+			if (tk->R==NULL && tk->L == NULL)
+				Cay = XOA_LA(Cay, tk);
+			else
+				if (tk->L!=NULL & tk->R == NULL)
+					Cay = XOA_TG_T(Cay, tk);
+				else
+					if (tk->L==NULL & tk->R != NULL)
+						Cay = XOA_TG_P(Cay, tk);
+					else
+						Cay = XOA_TG(Cay, tk);
+							
+	}
+	return Cay;
+}
+Node* deleteStudentsBelow5_5GPA(Node* root) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    root->L = deleteStudentsBelow5_5GPA(root->L);
+    root->R = deleteStudentsBelow5_5GPA(root->R);
+
+    if (root->sv.dtb < 5.5) {
+        root = XOA(root, root->sv);
+    }
+
+    return root;
+}
+
 
 int main() {
-	Node * root = NULL;
-    root=createBST(root);
-	printBST(root);
+    Node* root = NULL;
+    root = createBST(root);
+
+    printf("Binary Search Tree:\n");
+    printBST(root);
+
+    // Delete students with GPA below 5.5
+    root = deleteStudentsBelow5_5GPA(root);
+
+    printf("\nBinary Search Tree after deleting students with GPA below 5.5:\n");
+    printBST(root);
+
     return 0;
 }
 
